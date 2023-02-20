f1e8be530a66acd64608841e9c93f2ec21aa9c87
Jacob Handwerger <jstudyeth@gmail.com>
2023-02-20 15:01:51 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+
+import icon1 from './images/icon1.png';
+import icon2 from './images/icon2.png';
+import icon3 from './images/icon3.png';
+import icon4 from './images/icon4.png';
+import logo from './images/ironhack-logo-xs.png';
+import menu from './images/menu-top-xs.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className='body'>
+
+        <div className='nav'>
+          <img src={logo} alt="logo"/>
+          <img src={menu} alt="menu"/>
+        </div>
+
+        <div className='main'>
+          <h1>Say hello to react</h1>
+          <p>Become a react developer</p>
+        </div>
+
+        <div className='button'>
+          <button>Awesome!</button>
+        </div>
+
+      </div>
+
+      <div className='images'>
+
+        <div>
+          <img src={icon1} alt="icon1"/>
+
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+
+        <div>
+          <img src={icon2} alt="icon2"/>
+
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+
+        <div>
+          <img src={icon3} alt="icon3"/>
+
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+
+        <div>
+          <img src={icon4} alt="icon4"/>
+
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
